c40c4483c728bffbd4035c772e2ee1c80a739312
DOHUN256 <dohun5050@gmail.com>
2024-09-03 12:11:48 +0900

    add

---
+++ 2024-09-03/test3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int count = 999;
+    pritnf("처음에 저장한 초코 과자의 개수: %d개\n", count);
+
+    count = 99;
+    printf("변경한 초코 과자의 개수: %d개\n", count);
+}
---
+++ 2024-09-03/text4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char c = 'A';
+    printf("%C\n", c);
+
+    char c = 'B';
+    printf("%c\n", c);
+}
