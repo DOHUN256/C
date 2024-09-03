1b7e215519d6cee48c5dda5bfb5d6f233a13c880
DOHUN256 <dohun5050@gmail.com>
2024-09-03 12:04:46 +0900

    ADD

---
+++ 2024-09-03/test2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    double b;
+    char c;
+
+    a = 3;
+    b = 3.0002;
+    c= 'A';
+
+    printf("%d\n", a);
+    printf("%lf\n", b);
+    printf("%c\n", c);
+}             
