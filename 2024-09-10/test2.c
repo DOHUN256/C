3e2d9c35b7d29960237a4d45d2ae60111e74f84c
DOHUN256 <dohun5050@gmail.com>
2024-09-10 11:04:08 +0900

    add

---
+++ 2024-09-10/test2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char book = 'A';
+    
+    float score = 100;
+    
+    printf("이 %c은 %f점짜리 이다.", book, score);
+ 
+    return 0;
+}
---
+++ 2024-09-10/test3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int score;
+    printf("정현우의 C언어 책은 몇 점인지 입력하세요:");
+
+    scanf("%d",&score);
+
+    printf("n이 책은 %d점짜리 책이다.", score);
+
+    return 0;
+}
