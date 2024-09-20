2bf1eaf23753bbecba9e3203e3059aab64b202be
DOHUN256 <dohun5050@gmail.com>
2024-09-20 14:35:07 +0900

    add

--- 2024-09-20/test.c
+++ 2024-09-20/test.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    int score = 100;
+    int score;
+    scanf("%d",score);
     char book = '책';
-    printf("이 %c은 %f점짜리 이다.", book, score);
+    if(score >=90){
+        printf("1등급")
+    }
 }
+    if(score >=90){
+        printf("1등급");
+    }
+    if(90>score&& score>=70){
+        printf("2등급");
+    }
+    if(70 >score){
+        printf("2등급");
+
+        return 0;
+    }
